978091ef739aa41dcf28af177fcc62da0e4415dd
Justin Park <justin@Justins-MacBook-Pro.local>
2019-02-18 20:56:16 +0900

    Controller, H2 DB Test and httpRequest test source

---
+++ src/main/java/com/pulmuone/webservice/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.pulmuone.webservice.dto.posts;
+
+import com.pulmuone.webservice.domain.posts.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter //@RequestBody로 외부에서 데이터를 받는 경우엔 기본생성자 + set메소드를 통해서만 값이 할당
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
