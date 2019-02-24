799fa5f41b3f17d88cb192f8856beea2919be086
justin <justin.ad.park@gmail.com>
2019-02-24 20:24:30 +0900

    Change web server port to 80

--- src/main/java/com/pulmuone/webservice/Application.java
+++ src/main/java/com/pulmuone/webservice/Application.java
@@ -4,6 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+/**
+ *
+ * How to Test
+ *
+ * 1. http://localhost:8080/
+ *
+ *
+ * 2. H2 database test
+ *  http://localhost:8080/h2-console/
+ *  JDBC URL : jdbc:h2:mem:testdb
+ *
+ */
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
