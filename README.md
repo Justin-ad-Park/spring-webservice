a5c811e1cdd9d8d1864bef61a52e6942ee5e97da
Justin-ad-Park <justin.ad.park@gmail.com>
2019-02-26 14:50:36 +0900

    Add nginx profiles and update version 0.0.4

--- README.md
+++ README.md
@@ -4,3 +4,32 @@
 https://jojoldu.tistory.com/259?category=635883
 
 2019/2/24 6단계) Travis CI를 이용한 빌드까지 성공
+
+
+# 상용 application.yml을 형상관리 서버에 커밋하지 않고, 분리함.
+# 추후 spring configuration server로 이관을 통해 형상 관리 가능
+
+sudo mkdir -p /app/config/springboot-webservice
+sudo vim /app/config/springboot-webservice/real-application.yml
+
+#아래 소스로 구성함
+
+---
+spring:
+  profiles: set1
+server:
+  port: 8081
+
+---
+spring:
+  profiles: set2
+
+server:
+  port: 8082
+
+
+
+# Profile을 set1로 테스트 하려면...
+RUN > Edit Configuration 에서
+  VM에
+  -Dspring.profiles.active=set1
--- src/main/java/com/pulmuone/webservice/web/WebRestController.java
+++ src/main/java/com/pulmuone/webservice/web/WebRestController.java
@@ -4,11 +4,13 @@ import com.pulmuone.webservice.domain.posts.Posts;
 import com.pulmuone.webservice.dto.posts.PostsSaveRequestDto;
 import com.pulmuone.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -26,6 +28,9 @@ public class WebRestController {
         (@Autowired는 비권장)
      */
     private PostsService postsService;
+    private Environment env;
+
+
 
     @GetMapping("/hello")
     public String hello() {
@@ -35,5 +40,13 @@ public class WebRestController {
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto){
         postsService.save(dto);
+    }
+
+    // 현재 서비스 중인 프로파일 제공
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
